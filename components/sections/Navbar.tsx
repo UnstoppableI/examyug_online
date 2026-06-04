c957e6b8435e9e738f96f5ae97856cfdad8f5299
Aryan Agrawal <159540296+UnstoppableI@users.noreply.github.com>
2026-06-04 22:45:33 +0530

    Remove bg-accent class from Sign Up button

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -158,7 +158,7 @@ export function Navbar({ active, onLoginClick }: NavbarProps) {
 
             <Link href="/signup" className="w-full">
               <Button
-                className="w-full hover:scale-[1.02] bg-accent duration-300"
+                className="w-full hover:scale-[1.02] duration-300"
                 variant="outline"
               >
                 Sign Up
